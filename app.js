f646e3fbef6fe7ea5d4b584468baff9425b330e4
Creeksider <creeksider3@gmail.com>
2016-09-24 22:55:20 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express          = require("express"),
+    app              = express(),
+    bodyParser       = require("body-parser"),
+    expressSanitizer = require("express-sanitizer");
+    
+//requiring the only route
+var indexRoute = require("./routes/index"); //--
+
+    
+// APP CONFIG
+app.set("view engine", "ejs");
+app.use(express.static("public")); // TELLS EXPRESS TO LOOK FOR FILES IN THE 'PUBLIC' DIRECTORY --
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer()); // SANITIZES REQUEST BODIES ON ALL ROUTES
+
+
+
+
+
+//using the only route
+app.use("", indexRoute); //--
+
+
+
+// LISTEN FOR REQUESTS
+app.listen(process.env.PORT, process.env.IP, function(){
+  console.log("SERVER IS RUNNING!");
+});
+ 
---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+var smtp = process.env.SMTPURL
+// var smtp = 'smtps://elusivefoxy%40gmail.com:Rdbwyldstyle1@smtp.gmail.com';
+var transporter = nodemailer.createTransport(smtp);
+
+router.post("", function(req, res) {
+  
+  var name    = req.sanitize(req.body.message.name),
+      email   = req.sanitize(req.body.message.email),
+      subject = req.sanitize(req.body.message.subject),
+      body    = req.sanitize(req.body.message.body);
+      
+
+  var mailOptions = {
+    from: {
+      name: name,
+      address: email
+    }, // sender address
+    to: 'elusivefoxy@gmail.com', // list of receivers
+    subject: subject, // Subject line
+    text: body + '\n\n....................................................................\n\n' + email
+  };
+  
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log("Something went wrong while sending mail!");
+      console.log(error);
+      return res.render("../views/failure");
+    }
+    console.log('Message sent: ' + info.response);
+    res.render("../views/success");
+  });
+});
+
+
+
+
+
+module.exports = router;
